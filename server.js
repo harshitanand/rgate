61ffb96a80f87cf3b9183d335988bd5000c212c3
Harshit Anand <harshitanand94@gmail.com>
2021-06-06 23:12:32 +0530

    added a bootstrap HTTP server to proxy requests to different mico-containers

---
+++ server.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const express = require('express');
+const NodeCache = require('node-cache');
+
+const cache = new NodeCache();
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const cacheSet = (key) => {
+  const data = cache.get(key);
+  if (!data) cache.set(key, 1);
+  else cache.set(key, data + 1);
+};
+const requestMiddleware = (req, res, next) => {
+  if (req.url === '/stats') return next();
+  cacheSet('REQUEST_COUNT');
+  return next();
+};
+
+const responseMiddleware = (req, res, next) => {
+  if (req.url === '/stats') return next();
+
+  if (res.statusCode < 400) cacheSet('SUCCESS_COUNT');
+  else cacheSet('ERROR_COUNT');
+  return next();
+};
+
+const init = (port = 8080, proxies = []) => {
+  const app = express();
+
+  app.use(requestMiddleware);
+  app.use(responseMiddleware);
+
+  if (proxies.length > 0)
+    proxies.map((proxy) => {
+      app.use(
+        [`${proxy.route}/**`],
+        createProxyMiddleware({
+          target: proxy.target,
+          pathRewrite: {
+            [`^${proxy.route}`]: '',
+          },
+          changeOrigin: true,
+          // onProxyReq: (err, req, res, next) => requestMiddleware(req, res, next),
+          // onProxyRes: (err, req, res, next) => responseMiddleware(req, res, next),
+        })
+      );
+      return proxy;
+    });
+
+  app.get('/stats', (req, res) => {
+    return res.status(200).json({
+      requests: {
+        success: cache.get('SUCCESS_COUNT') || 0,
+        error: cache.get('ERROR_COUNT') || 0,
+        total: cache.get('REQUEST_COUNT') || 0,
+      },
+    });
+  });
+
+  app.get('/api', (req, res) => res.status(200).json({ messages: 'done' }));
+
+  process.on('SIGTERM', () => {
+    app.close(() => {
+      cache.flushAll();
+      console.log('Process terminated');
+    });
+  });
+
+  app.listen(port);
+};
+
+module.exports = {
+  init,
+};
